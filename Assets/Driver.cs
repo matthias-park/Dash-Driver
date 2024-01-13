4bb17ed7533cafd570f17b71fa7ae9d667be9258
Joons <matthiaspark0514@gmail.com>
2024-01-12 16:38:42 -0800

    Test: Transform, Input and Time Method

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,45);
+        float steerAmount = UnityEngine.Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = UnityEngine.Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+        
     }
 }
